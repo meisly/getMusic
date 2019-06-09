1342480f207c8e0e81323a07547c97dfb9cf6390
Tierra <sr.undershaft@gmail.com>
2019-06-09 13:11:11 -0500

    cleaned up comments

--- librarySwap.py
+++ librarySwap.py
@@ -169,14 +169,5 @@ if __name__ == "__main__":
 
     
 
-    # test = ['Tagrdsjvhn5njasvchcrlnhzx6y', 'Tl46ftluata3xgecexjpdxqpxva', 'T3xpfno3xrj5mxveuozeebzuaiq', 'Thjebjnyitc2oigfbv5ljelstvq', 'Tojowhwp3q5orw7bhmgqlzwzgma', 'T7o5j6lhzrz5y4yzq7uv3ch4psi', 'Tcgl5oure3pprjje5dyhie42fmm', 'Tsmdmnzkinvjqc4yymrlhxrxzny', 'Tnxszfyeajgoefaeml2ye44qk5m', 'Tg7daojc7ahuhbxfhusjzpmdlqu', 'Tje26qvz3wfnmoorpaucsblco4y', 'Tlav5j4tuxvi45q4hos4j5gvwii', 'Tyg6hoe3lstmlu4zdm7gl4hqyni', 'Tygfvtuwsbwfnajyad7lvpln3sq', 'Thpjsdqkhn67wbgt7wqkcxaa5be', 'Tdc3zg55sabyd2wp6t5vckoll3m', 'Tum7lsimn2jd37enim77wzenqsa', 'Tqdwwyibd2rgx4f7gsse2rqs24i', 'Tj6mhocdb5xlexa3xgpxd6wqgse', 'Thawjnnegidasxzfg5jhtvlfmka', 'Tuz77zzjclsu6qyo3t3nn6ctpnm', 'T26aktvztvursmbw5rcduf6pb3e', 'Tvwtkhzxkc3ibnyxmra7hmet3pe', 'Tjiovfdaqcktkweej2j2vtntnw4', 'Tslbornnieprppu2hptf5sxulty', 'Tnubdym3x44xukjfsjhd7f4crdy', 'Tvaqb7zzrh6qwpzgvbo6qtup4au', 'Tvifh2gqfezi7a7qgavkb2cwjwa', 'Txuxja2jos7n2k6l3527qri2y7q', 'Teuaui6zgbin2dhmuf2rfru42yi', 'Tddels6wnksctnuf5mmz2ro2wnm', 'T4uvj37ftqafv56d752mltl2iqm', 'Teamhpks2b2jbfe62tz62vz4dnm', 'Tm3ligtwkczzaxp75wehzinxsau', 'Tftqmvubieoscuvjspjrimykh6m', 'Tergzlxddllc2eehpkabzb5uzmq', 'T4cxvsbllhkn2s7rdeax2be7dmm']
-
-    #print found and missed tracks to file
-    # if input("Would you like to save  (Y/N):") == 'Y':
-    #     today = str(datetime.now()).replace('-', '').replace(' ','').replace(':','').replace('.','')  # cause i'm too lazy for regex
-    #     with open('missedTracks' + today + '.json','w') as responseFile:
-    #         json.dump(fails, responseFile)
-    #     with open('trackHits' + today + '.json','w') as reFile:
-    #         json.dump(songIDs, reFile)
 
 
